b32147468c59e3d0bcd502af787403a30b11f719
Alan Ayoub <alanayoub@me.com>
2023-01-18 14:53:12 -0800

    netflix 2.0 login page almost done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import './HomeScreen';
+import './screens/HomeScreen';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import './App.css';
-import HomeScreen from './HomeScreen';
+import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/LoginScreen';
 
 function App() {
+  const user = null;
   return (
     <div className="app">
-      <HomeScreen />
-    </div>
+      <Router>
+        {!user ? (
+          <LoginScreen />
+        ) : (
+          <Routes>
+            <Route path='/' element={<HomeScreen />} />
+            <Route path='/test' element={<h1>This is just a TEST</h1>} />
+          </Routes>
+        )}
+      </Router>
+    </div >
   );
 }
 
